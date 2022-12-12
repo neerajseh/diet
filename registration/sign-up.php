2486f119e936b52f674a6225552850283c61e15e
Neeraj sehgal <106014248+neerajseh@users.noreply.github.com>
2022-12-12 13:43:15 +0530

    files of diet

---
+++ registration/sign-up.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>GFG- Store Data</title>
+	<link rel="stylesheet" href="regis.css">
+</head>
+<body>
+	<center>
+		<h1>sign-up</h1>
+		<form action="dbconnect.php" method="post">
+			
+<p>
+			<label for="firstName">First Name:</label>
+			<input type="text" name="first_name" id="firstName">
+			</p>
+
+			
+<p>
+			<label for="lastName">Last Name:</label>
+			<input type="text" name="last_name" id="lastName">
+			</p>
+
+			
+<p>
+			<label for="Gender">Gender:</label>
+			<input type="text" name="gender" id="Gender">
+			</p>
+
+			
+<p>
+			<label for="Address">Address:</label>
+			<input type="text" name="address" id="Address">
+			</p>
+
+			
+<p>
+			<label for="emailAddress">Email Address:</label>
+			<input type="text" name="email" id="emailAddress">
+			</p>
+
+			<input class="btn" type="submit" value="Submit" style="
+    width: 376.4px;
+">
+		</form>
+	</center>
+</body>
+</html>
